9e4afb993b868d8e6c7be7a064395d747ecc0342
Tamim khan <mdtamimkhan842@gmail.com>
2023-12-31 13:08:19 +0600

    second commit

---
+++ B - DNA Sequence .cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+typedef long long int ll;
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+int32_t main()
+{
+    MTK;
+    int n;
+    string s;
+    cin >> n >> s;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if ((s[i] == 'G' and s[i + 1] == 'C') || (s[i] == 'C' and s[i + 1] == 'G'))
+            ans++;
+        else if ((s[i] == 'A' and s[i + 1] == 'T') || (s[i] == 'T' and s[i + 1] == 'A'))
+            ans++;
+    }
+    cout << ans+1 << '\n';
+}
