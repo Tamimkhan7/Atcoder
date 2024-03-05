a407dd41c4c54f17016632deb550cf44eb54dfaa
Tamim khan <mdtamimkhan842@gmail.com>
2024-03-06 02:24:07 +0600

    added atcoder contest solving problem

--- contest/Diversity of Scores .cpp
+++ contest/Diversity of Scores .cpp
@@ -1,31 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
-#define MTK                       \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
+#define MTK                     \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
 typedef long long int ll;
 #define all(x) x.begin(), x.end()
 #define mod 1000000007
 
 int32_t main()
 {
-    MTK;
-    int n,p;
-    cin >>p>>n;
-   map<int,int>mp;
- for(int i=1; i<=p; i++)mp[i]=0;
- while(n--){
-  int a,b;cin>>a>>b;
-  mp[a]=b;
-  for(auto [x,y]:mp)cout<<x<<' '<<y<<'\n';
-  set<int>s;
-  for(auto [x,y]: mp)
-    s.insert(y);
-    for(auto x: s)cout<<x<<'\n';
-  
-  cout<<s.size()<<'\n';
-
- }
+  MTK;
+  int n, p;
+  cin >> p >> n;
+  map<int, int> mp;
+  for (int i = 1; i <= p; i++)
+    mp[i] = 0;
+  while (n--)
+  {
+    int a, b;
+    cin >> a >> b;
+    if (mp.find(a) != mp.end())
+    {
+      int x = mp[a];
+      b += x;
+    }
+    mp[a] = b;
+    set<int> s;
+    for (auto [x, y] : mp)
+      s.insert(y);
+    cout << s.size() << '\n';
+  }
 }
---
+++ contest/F - Second Largest Query.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+typedef long long int ll;
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+int n, q;
+vector<int> v;
+void solve()
+{
+    while (q--)
+    {
+        int type, a, b;
+        cin >> type >> a >> b;
+        if (type == 1)
+            v[a - 1] = b;
+        else
+        {
+            vector<int> ans;
+            a--;
+            for (int i = a; i < b; i++)
+            {
+                int value = v[i];
+                ans.push_back(value);
+            }
+            sort(all(ans), greater<int>());
+            int mn = ans[0], px = -1;
+            for (int i = 0; i < ans.size(); i++)
+            {
+                if (ans[i] != mn)
+                {
+                    px = ans[i];
+                    break;
+                }
+            }
+
+            int occurrences_cnt = count(all(ans), px);
+            if (px == -1)
+                cout << 0 << '\n';
+            else
+                cout << occurrences_cnt << '\n';
+        }
+    }
+}
+int32_t main()
+{
+    MTK;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    solve();
+}
+// total_time(n*q*nlogn*nlongn*nlongn)
