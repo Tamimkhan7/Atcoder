97f13ae368d6644ef2f744f7b6946dde56ed744b
Tamim khan <mdtamimkhan842@gmail.com>
2025-01-11 22:25:14 +0600

    added contest 388

---
+++ Beginner Contest 388)/A - ?UPC.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll int long long
+
+int32_t main()
+{
+    MTK;
+    string s;
+    cin >> s;
+    char first = s[0];
+    cout << s[0] << "UPC" << '\n';
+    return 0;
+}
